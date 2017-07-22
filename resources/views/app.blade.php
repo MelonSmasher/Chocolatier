546a90f397bf47955e1f7241dc302dd190289c93
Alex Markessinis <markea@sage.edu>
2017-07-21 23:58:21 -0400

    added logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,8 +1,27 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="apple-touch-icon-57x57.png"/>
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="apple-touch-icon-114x114.png"/>
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="apple-touch-icon-72x72.png"/>
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="apple-touch-icon-144x144.png"/>
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="apple-touch-icon-60x60.png"/>
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="apple-touch-icon-120x120.png"/>
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="apple-touch-icon-76x76.png"/>
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="apple-touch-icon-152x152.png"/>
+    <link rel="icon" type="image/png" href="favicon-196x196.png" sizes="196x196"/>
+    <link rel="icon" type="image/png" href="favicon-96x96.png" sizes="96x96"/>
+    <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32"/>
+    <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16"/>
+    <link rel="icon" type="image/png" href="favicon-128.png" sizes="128x128"/>
+    <meta name="application-name" content="&nbsp;"/>
+    <meta name="msapplication-TileColor" content="#FFFFFF"/>
+    <meta name="msapplication-TileImage" content="mstile-144x144.png"/>
+    <meta name="msapplication-square70x70logo" content="mstile-70x70.png"/>
+    <meta name="msapplication-square150x150logo" content="mstile-150x150.png"/>
+    <meta name="msapplication-wide310x150logo" content="mstile-310x150.png"/>
+    <meta name="msapplication-square310x310logo" content="mstile-310x310.png"/>
     <title>{{ config('choco.shortname') }} - @yield('title')</title>
-
     <link href="{{ elixir('css/app.css') }}" rel="stylesheet" type='text/css'>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Let browser know website is optimized for mobile-->
@@ -55,14 +74,15 @@
             </div>
             <div class="col l4 offset-l2 s12">
                 @if(!empty(config('choco.links')) && config('choco.display_links'))
-                <h5 class="white-text">Links</h5>
-                <ul>
-                    @foreach(config('choco.links') as $link)
-                        <li>
-                            <a class="grey-text text-lighten-3" href="{{ $link['href'] }}">{!! $link['title'] !!}</a>
-                        </li>
-                    @endforeach
-                </ul>
+                    <h5 class="white-text">Links</h5>
+                    <ul>
+                        @foreach(config('choco.links') as $link)
+                            <li>
+                                <a class="grey-text text-lighten-3"
+                                   href="{{ $link['href'] }}">{!! $link['title'] !!}</a>
+                            </li>
+                        @endforeach
+                    </ul>
                 @endif
             </div>
         </div>
@@ -79,12 +99,12 @@
 <script type="text/javascript" src="/js/bin/materialize.js"></script>
 
 <script>
-    $(function () {
-        // Initialize collapse button
-        $(".button-collapse").sideNav();
-        // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-        // $('.collapsible').collapsible();
-    });
+  $(function () {
+    // Initialize collapse button
+    $(".button-collapse").sideNav();
+    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+    // $('.collapsible').collapsible();
+  });
 </script>
 </body>
 </html>
