944e43110eba84cb9982506634d8a4e6314dca3d
Alex Markessinis <markea@sage.edu>
2017-07-21 22:16:52 -0400

    fixed api route, api in the url was redundant

--- routes/api.php
+++ routes/api.php
@@ -6,35 +6,30 @@
  * Time: 7:17 PM
  */
 
-Route::group(['as' => 'api.'], function ()
-{
-    Route::group(['middleware' => ['auth.nuget', 'file.nuget:package']], function ()
-    {
+
+Route::group(['middleware' => ['auth.nuget', 'file.nuget:package']], function () {
+    Route::put('/upload', ['as' => 'upload', 'uses' => 'ApiController@upload']);
+    Route::put('/', ['as' => 'upload', 'uses' => 'ApiController@upload']);
+});
+
+Route::get('/download/{id}/{version}', ['as' => 'download', 'uses' => 'ApiController@download']);
+
+Route::group(['prefix' => '/api/v2'], function () {
+    Route::group(['middleware' => ['auth.nuget', 'file.nuget:package']], function () {
         Route::put('/upload', ['as' => 'upload', 'uses' => 'ApiController@upload']);
         Route::put('/', ['as' => 'upload', 'uses' => 'ApiController@upload']);
+        Route::put('/package', ['as' => 'upload', 'uses' => 'ApiController@upload']);
     });
 
-    Route::get('/download/{id}/{version}', ['as' => 'download', 'uses' => 'ApiController@download']);
-
-    Route::group(['prefix' => '/api/v2'], function ()
-    {
-        Route::group(['middleware' => ['auth.nuget', 'file.nuget:package']], function ()
-        {
-            Route::put('/upload', ['as' => 'upload', 'uses' => 'ApiController@upload']);
-            Route::put('/', ['as' => 'upload', 'uses' => 'ApiController@upload']);
-            Route::put('/package', ['as' => 'upload', 'uses' => 'ApiController@upload']);
-        });
-
-        Route::get('/', ['as' => 'index', 'uses' => 'ApiController@index']);
-        Route::get('$metadata', ['as' => 'metadata', 'uses' => 'ApiController@metadata']);
-        Route::get('Packages()', ['as' => 'packages', 'uses' => 'ApiController@packages']);
-        Route::get('Packages', ['as' => 'packages', 'uses' => 'ApiController@packages']);
-        Route::get('GetUpdates()', ['as' => 'updates', 'uses' => 'ApiController@updates']);
-        Route::get('GetUpdates', ['as' => 'updates', 'uses' => 'ApiController@updates']);
-        Route::get('Search()/{action}', ['as' => 'search.action', 'uses' => 'ApiController@search']);
-        Route::get('Search()', ['as' => 'search', 'uses' => 'ApiController@searchNoAction']);
-        Route::get('Search', ['as' => 'search', 'uses' => 'ApiController@searchNoAction']);
-        Route::get('FindPackagesById()', ['as' => 'findById', 'uses' => 'ApiController@packages']);
-        Route::get('Packages(Id=\'{id}\',Version=\'{version}\')', ['as' => 'package', 'uses' => 'ApiController@package']);
-    });
+    Route::get('/', ['as' => 'index', 'uses' => 'ApiController@index']);
+    Route::get('$metadata', ['as' => 'metadata', 'uses' => 'ApiController@metadata']);
+    Route::get('Packages()', ['as' => 'packages', 'uses' => 'ApiController@packages']);
+    Route::get('Packages', ['as' => 'packages', 'uses' => 'ApiController@packages']);
+    Route::get('GetUpdates()', ['as' => 'updates', 'uses' => 'ApiController@updates']);
+    Route::get('GetUpdates', ['as' => 'updates', 'uses' => 'ApiController@updates']);
+    Route::get('Search()/{action}', ['as' => 'search.action', 'uses' => 'ApiController@search']);
+    Route::get('Search()', ['as' => 'search', 'uses' => 'ApiController@searchNoAction']);
+    Route::get('Search', ['as' => 'search', 'uses' => 'ApiController@searchNoAction']);
+    Route::get('FindPackagesById()', ['as' => 'findById', 'uses' => 'ApiController@packages']);
+    Route::get('Packages(Id=\'{id}\',Version=\'{version}\')', ['as' => 'package', 'uses' => 'ApiController@package']);
 });
