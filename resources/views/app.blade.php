3cc4152c175e303b7ec8ede666f51655a86d61c9
Alex Markessinis <markea125@gmail.com>
2019-07-27 00:11:18 -0400

    recompiled style assets

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -23,7 +23,7 @@
     <meta name="msapplication-square310x310logo" content="mstile-310x310.png"/>
 
     <title>{{ config('choco.shortname') }} - @yield('title')</title>
-    <link href="{{ elixir('css/app.css') }}" rel="stylesheet" type='text/css'>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet" type='text/css'>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
