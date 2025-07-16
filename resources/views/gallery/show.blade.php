55b6c545c9cd0cc20959d6eab0bdc09ec27a5391
Alex Markessinis <alex@markessinis.me>
2025-07-16 19:31:45 -0400

    fixing choco install

--- resources/views/gallery/show.blade.php
+++ resources/views/gallery/show.blade.php
@@ -14,7 +14,8 @@
             </div>
             <div class="col m4">
                 <div class="img-wrapper" style="max-width: 150px;">
-                    <img class="responsive-img" src="{{ $package->getIconUrl() }}" alt="{{ $package->package_id }}" onerror="this.src='{{asset('images/packageDefaultIcon.png')}}';" />
+                    <img class="responsive-img" src="{{ $package->getIconUrl() }}" alt="{{ $package->package_id }}"
+                         onerror="this.src='{{asset('images/packageDefaultIcon.png')}}';"/>
                 </div>
                 <strong>{{ $package->download_count }} downloads</strong><br/>
                 <strong>{{ $package->version_download_count }} downloads of {{ $package->version }}</strong><br/>
@@ -31,24 +32,26 @@
             @if(!empty($package->release_notes))
                 <h5>Release Notes</h5>
                 <p>
-                    <pre>{{ $package->release_notes }}</pre>
+                <pre>{{ $package->release_notes }}</pre>
                 </p>
             @endif
 
-            <ul class="collapsible">
-                <li>
-                    <div class="collapsible-header">
-                        <h5><i class="material-icons">keyboard_arrow_down</i> Install {{ $package->title }}</h5>
-                    </div>
-                    <div class="collapsible-body">
-                        Install the latest version of {{ $package->title }}:
-                        <pre><code class="language-powershell">choco install {{ $package->package_id }}</code></pre>
+            <div class="col m4">
+                <ul class="collapsible">
+                    <li>
+                        <div class="collapsible-header">
+                            <h5><i class="material-icons">keyboard_arrow_down</i> Install {{ $package->title }}</h5>
+                        </div>
+                        <div class="collapsible-body">
+                            Install the latest version of {{ $package->title }}:
+                            <pre><code class="language-powershell">choco install {{ $package->package_id }}</code></pre>
 
-                        Install the latest version {{$package->version}} of {{ $package->title }}:
-                        <pre><code class="language-powershell">choco install {{ $package->package_id }} --version {{ $package->version }}</code></pre>
-                    </div>
-                </li>
-            </ul>
+                            Install the latest version {{$package->version}} of {{ $package->title }}:
+                            <pre><code class="language-powershell">choco install {{ $package->package_id }} --version {{ $package->version }}</code></pre>
+                        </div>
+                    </li>
+                </ul>
+            </div>
 
             @if(!empty($package->getOwners()))
                 <h5>Owners</h5>
@@ -80,8 +83,9 @@
                 <h5>Dependencies</h5>
                 <ul>
                     @foreach(explode('|', $package->dependencies) as $dependency)
-                        <?php $split = explode(':', $dependency); ?>
-                        <li><a href="{{route('packages.show', $split[0]) }}">{{ $split[0] }}</a> (&ge; {{ $split[1] }})</li>
+                            <?php $split = explode(':', $dependency); ?>
+                        <li><a href="{{route('packages.show', $split[0]) }}">{{ $split[0] }}</a> (&ge; {{ $split[1] }})
+                        </li>
                     @endforeach
                 </ul>
             @endif
@@ -89,39 +93,39 @@
             <h5>Version History</h5>
             <table class="striped">
                 <thead>
-                    <tr>
-                        <th>
-                            Version
-                        </th>
-                        <th>
-                            Downloads
-                        </th>
-                        <th>
-                            Last Updated
-                        </th>
-                    </tr>
+                <tr>
+                    <th>
+                        Version
+                    </th>
+                    <th>
+                        Downloads
+                    </th>
+                    <th>
+                        Last Updated
+                    </th>
+                </tr>
                 </thead>
                 <tbody>
-                    @foreach($versions as  $version)
-                        <tr class="{{ $version->version == $package->version ? 'bold' : '' }}">
-                            <td>
-                                <a href="{{route('packages.version.show', ['name' => $package->package_id, 'version' => $version->version])}}">{{ $version->version }}
+                @foreach($versions as  $version)
+                    <tr class="{{ $version->version == $package->version ? 'bold' : '' }}">
+                        <td>
+                            <a href="{{route('packages.version.show', ['name' => $package->package_id, 'version' => $version->version])}}">{{ $version->version }}
                                 @if($version->version == $package->version)
                                     (this version)
                                 @endif
                                 @if($version->is_latest_version)
                                     (latest stable)
                                 @endif
-                                </a>
-                            </td>
-                            <td>
-                                {{ $version->version_download_count }}
-                            </td>
-                            <td>
-                                {{ $version->created_at->toFormattedDateString() }}
-                            </td>
-                        </tr>
-                    @endforeach
+                            </a>
+                        </td>
+                        <td>
+                            {{ $version->version_download_count }}
+                        </td>
+                        <td>
+                            {{ $version->created_at->toFormattedDateString() }}
+                        </td>
+                    </tr>
+                @endforeach
                 </tbody>
             </table>
         </div>
