abd718e0ff617ff4eaa4d96477a1720f1c282e42
Alex Markessinis <alex@markessinis.me>
2025-07-16 17:21:44 -0400

    added choco install script

--- resources/views/gallery/home.blade.php
+++ resources/views/gallery/home.blade.php
@@ -12,7 +12,22 @@
                 <p>
                     {{Config::get('choco.shortname')}} is a NuGet package manager repository server.
                     To browse this repository, add <code>{{ route('api.index') }}</code> to your NuGet
-                    Package Manager configuration.
+                    Package Manager configuration:
+
+                    <pre>
+                        <code class="language-powershell">
+                            @if (Config::get('app.site_user') != null && Config::get('app.site_password') != null)
+                                choco source add -n "{{strtolower(trim(Config::get('choco.shortname')))}}" -s "{{ route('api.index') }}" -u "{{ Config::get('app.site_user') }}" -p "{{ Config::get('app.site_password') }}"
+                            @elseif (Config::get('app.site_user') != null)
+                                choco source add -n "{{strtolower(trim(Config::get('choco.shortname')))}}" -s "{{ route('api.index') }}" -u "{{ Config::get('app.site_user') }}" -p ""
+                            @elseif (Config::get('app.site_password') != null)
+                                choco source add -n "{{strtolower(trim(Config::get('choco.shortname')))}}" -s "{{ route('api.index') }}" -u "" -p "{{ Config::get('app.site_password') }}"
+                            @else
+                                choco source add -n "{{strtolower(trim(Config::get('choco.shortname')))}}" -s "{{ route('api.index') }}"
+                            @endif
+                        </code>
+                    </pre>
+
                 </p>
             </div>
         </div>
