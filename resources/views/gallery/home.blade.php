61c71f1fa2c98397cbe897637d48562175406cd7
Alex Markessinis <alex@markessinis.me>
2025-07-18 11:49:53 -0400

    added admin only command to source add

--- resources/views/gallery/home.blade.php
+++ resources/views/gallery/home.blade.php
@@ -32,6 +32,16 @@
                             @else
                                 <pre><code class="language-powershell">choco source add -n "{{strtolower(trim(Config::get('choco.shortname')))}}" -s "{{ route('api.index') }}"</code></pre>
                             @endif
+                            Add the new source (Admin Only Repository):
+                            @if (Config::get('app.site_user') != '' && Config::get('app.site_password') != '')
+                                <pre><code class="language-powershell">choco source add -n "{{strtolower(trim(Config::get('choco.shortname')))}}" -s "{{ route('api.index') }}" -u "{{ Config::get('app.site_user') }}" -p "{{ Config::get('app.site_password') }}" --adminonly</code></pre>
+                            @elseif (Config::get('app.site_user') != '')
+                                <pre><code class="language-powershell">choco source add -n "{{strtolower(trim(Config::get('choco.shortname')))}}" -s "{{ route('api.index') }}" -u "{{ Config::get('app.site_user') }}" -p "" --adminonly</code></pre>
+                            @elseif (Config::get('app.site_password') != '')
+                                <pre><code class="language-powershell">choco source add -n "{{strtolower(trim(Config::get('choco.shortname')))}}" -s "{{ route('api.index') }}" -u "" -p "{{ Config::get('app.site_password') }}" --adminonly</code></pre>
+                            @else
+                                <pre><code class="language-powershell">choco source add -n "{{strtolower(trim(Config::get('choco.shortname')))}}" -s "{{ route('api.index') }}" --adminonly</code></pre>
+                            @endif
                         </div>
                     </li>
                 </ul>
@@ -39,7 +49,8 @@
                 <ul class="collapsible">
                     <li>
                         <div class="collapsible-header">
-                            <h5><i class="material-icons">keyboard_arrow_down</i> Chocolatey Installation Instructions</h5>
+                            <h5><i class="material-icons">keyboard_arrow_down</i> Chocolatey Installation Instructions
+                            </h5>
                         </div>
                         <div class="collapsible-body">
                             To install chocolatey from scratch:
@@ -53,19 +64,29 @@
                                 <pre><code class="language-powershell">Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('{{route('api.index')}}/ChocolateyInstall.ps1'))</code></pre>
                             @endif
 
-                        Delete the existing default source:
-                        <pre><code class="language-powershell">choco source remove -n chocolatey</code></pre>
+                            Delete the existing default source:
+                            <pre><code class="language-powershell">choco source remove -n chocolatey</code></pre>
 
-                        Add the new source:
-                        @if (Config::get('app.site_user') != '' && Config::get('app.site_password') != '')
-                            <pre><code class="language-powershell">choco source add -n "{{strtolower(trim(Config::get('choco.shortname')))}}" -s "{{ route('api.index') }}" -u "{{ Config::get('app.site_user') }}" -p "{{ Config::get('app.site_password') }}"</code></pre>
-                        @elseif (Config::get('app.site_user') != '')
-                            <pre><code class="language-powershell">choco source add -n "{{strtolower(trim(Config::get('choco.shortname')))}}" -s "{{ route('api.index') }}" -u "{{ Config::get('app.site_user') }}" -p ""</code></pre>
-                        @elseif (Config::get('app.site_password') != '')
-                            <pre><code class="language-powershell">choco source add -n "{{strtolower(trim(Config::get('choco.shortname')))}}" -s "{{ route('api.index') }}" -u "" -p "{{ Config::get('app.site_password') }}"</code></pre>
-                        @else
-                            <pre><code class="language-powershell">choco source add -n "{{strtolower(trim(Config::get('choco.shortname')))}}" -s "{{ route('api.index') }}"</code></pre>
-                        @endif
+                            Add the new source:
+                            @if (Config::get('app.site_user') != '' && Config::get('app.site_password') != '')
+                                <pre><code class="language-powershell">choco source add -n "{{strtolower(trim(Config::get('choco.shortname')))}}" -s "{{ route('api.index') }}" -u "{{ Config::get('app.site_user') }}" -p "{{ Config::get('app.site_password') }}"</code></pre>
+                            @elseif (Config::get('app.site_user') != '')
+                                <pre><code class="language-powershell">choco source add -n "{{strtolower(trim(Config::get('choco.shortname')))}}" -s "{{ route('api.index') }}" -u "{{ Config::get('app.site_user') }}" -p ""</code></pre>
+                            @elseif (Config::get('app.site_password') != '')
+                                <pre><code class="language-powershell">choco source add -n "{{strtolower(trim(Config::get('choco.shortname')))}}" -s "{{ route('api.index') }}" -u "" -p "{{ Config::get('app.site_password') }}"</code></pre>
+                            @else
+                                <pre><code class="language-powershell">choco source add -n "{{strtolower(trim(Config::get('choco.shortname')))}}" -s "{{ route('api.index') }}"</code></pre>
+                            @endif
+                            Add the new source (Admin Only Repository):
+                            @if (Config::get('app.site_user') != '' && Config::get('app.site_password') != '')
+                                <pre><code class="language-powershell">choco source add -n "{{strtolower(trim(Config::get('choco.shortname')))}}" -s "{{ route('api.index') }}" -u "{{ Config::get('app.site_user') }}" -p "{{ Config::get('app.site_password') }}" --adminonly</code></pre>
+                            @elseif (Config::get('app.site_user') != '')
+                                <pre><code class="language-powershell">choco source add -n "{{strtolower(trim(Config::get('choco.shortname')))}}" -s "{{ route('api.index') }}" -u "{{ Config::get('app.site_user') }}" -p "" --adminonly</code></pre>
+                            @elseif (Config::get('app.site_password') != '')
+                                <pre><code class="language-powershell">choco source add -n "{{strtolower(trim(Config::get('choco.shortname')))}}" -s "{{ route('api.index') }}" -u "" -p "{{ Config::get('app.site_password') }}" --adminonly</code></pre>
+                            @else
+                                <pre><code class="language-powershell">choco source add -n "{{strtolower(trim(Config::get('choco.shortname')))}}" -s "{{ route('api.index') }}" --adminonly</code></pre>
+                            @endif
                         </div>
                     </li>
                 </ul>
